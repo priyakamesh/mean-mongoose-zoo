915e7d6080d3fc70154f0327c095d03b0434cb2f
krishnapriya <krishnapriyakamesh@gmail.com>
2017-04-24 15:21:33 -0500

    added catch for zookeeper functions

--- client/app/factories/ZookeeperFact.js
+++ client/app/factories/ZookeeperFact.js
@@ -4,26 +4,29 @@ app.factory('ZookeeperFact', function($http){
     return {
     getAll: function() {
         return new Promise((resolve, reject) =>{
-          $http.get(`http://localhost:3000/api/allZookeepers`)
+          $http.get(`https://zoo-api.herokuapp.com/api/v1/zookeepers`)
             .then((data) => {
               resolve(data.data)
             })
+            .catch(err => console.log(err))
         })
       },
     add: function(newZookeeper) {
       return new Promise((resolve, reject) =>{
-        $http.post(`http://localhost:3000/api/addZookeeper`, newZookeeper)
+        $http.post(`https://zoo-api.herokuapp.com/api/v1/zookeepers/new`, newZookeeper)
           .then((data) => {
             resolve(data.data.zookeepers)
           })
+          .catch(err => console.log(err))
         })
     },
     delete: function(id) {
       return new Promise((resolve, reject) => {
-        $http.delete(`http://localhost:3000/api/zookeeper/${id}`)
+        $http.delete(`https://zoo-api.herokuapp.com/api/v1/zookeepers/${id}`)
           .then((data) => {
             resolve()
           })
+          .catch(err => console.log(err))
       })
     }
   }
