a2f918f44f9c4e315d56b469ca1432cbb568db82
krishnapriya <krishnapriyakamesh@gmail.com>
2017-04-26 11:13:55 -0500

    added tricks features to add animals html

--- client/app/factories/TrickFact.js
+++ client/app/factories/TrickFact.js
@@ -1,28 +1,28 @@
-app.factory('TrainerFact', function($http){
-  console.log("here's the trainer factory")
+app.factory('TrickFact', function($http){
+  console.log("here's the trick factory")
 
     return {
     getAll: function() {
         return new Promise((resolve, reject) =>{
-          $http.get(`http://localhost:3000/api/v1/trainers`)
+          $http.get(`http://localhost:3000/api/v1/tricks`)
             .then((data) => {
               resolve(data.data)
             })
             .catch(err => console.log(err))
         })
       },
-    add: function(newTrainer) {
+    add: function(newTrick) {
       return new Promise((resolve, reject) =>{
-        $http.post(`http://localhost:3000/api/v1/trainers/new`, newTrainer)
+        $http.post(`http://localhost:3000/api/v1/tricks/new`, newTrick)
           .then((data) => {
-            resolve(data.data.trainers)
+            resolve(data.data.tricks)
           })
           .catch(err => console.log(err))
         })
     },
     delete: function(id) {
       return new Promise((resolve, reject) => {
-        $http.delete(`http://localhost:3000/api/v1/trainers/${id}`)
+        $http.delete(`http://localhost:3000/api/v1/tricks/${id}`)
           .then((data) => {
             resolve()
           })
